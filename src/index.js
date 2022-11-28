8b73503548e2e57a47a72973e64b6eb897980e44
Rodrigo Jiménez <rodrigo.jimenez@alumnos.ucn.cl>
2022-11-28 16:03:11 -0300

    feat: added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line no-unused-vars
+import firebase from "./utils/firebase"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
